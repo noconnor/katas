072c32926956a4a1e39da055bf5bcd223ea6a017
noconnor <noconnorie@protonmail.com>
2018-05-28 15:52:09 +0100

    Adding bubble sort algorithm

--- kata_sort/README.md
+++ kata_sort/README.md
@@ -1,7 +1,21 @@
 ## Objective 
 
-1. Implement bubble sort algorithm
+1. [Implement bubble sort algorithm](src/main/java/com/github/noconnor/reference/BubbleSort.java)
 2. Implement quick sort algorithm
 3. Implement merge sort algorithm
 
+## Complexity
+
+https://en.wikipedia.org/wiki/Sorting_algorithm#Comparison_of_algorithms
+
+
+|Algorithm| Time - Worst case| Time - Best case | Time - Average | Space - Worst case|
+|:---|:---|:---|:---|:---|
+|Bubble Sort|O(n^2)|O(n)|O(n^2)|O(1)| 
+|Merge Sort|||||
+|Quick Sort|||||
+
 ## Reference
+
+* [Bubble Sort](https://en.wikipedia.org/wiki/Sorting_algorithm#Bubble_sort)
+  * [Bubble sort java reference](https://www.mkyong.com/java/java-bubble-sort-example/) 
--- kata_sort/src/main/java/com/github/noconnor/reference/BubbleSort.java
+++ kata_sort/src/main/java/com/github/noconnor/reference/BubbleSort.java
@@ -1,5 +1,42 @@
 package com.github.noconnor.reference;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+
 public class BubbleSort {
 
+    private int interations;
+
+    public void sort(int[] array) {
+        int n = array.length;
+        interations = 0;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (array[i - 1] > array[i]) {
+                    int buffer = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = buffer;
+                    swapped = true;
+                }
+                interations++;
+            }
+
+        } while (!swapped);
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{12, 3, 3, 4, 5, 7, 8, 99, 23, 8};
+
+        BubbleSort bs = new BubbleSort();
+        System.out.println("Before: " + Arrays.toString(array));
+        bs.sort(array);
+        System.out.println("After: " + Arrays.toString(array) + "[iterations:" + bs.interations + "]");
+
+        assertThat(bs.interations, is(9));
+        assertThat(array, is(new int[]{3, 3, 4, 5, 7, 8, 12, 23, 8, 99}));
+    }
 }
