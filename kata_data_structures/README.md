50c6d092ca882a3d22415f9c8bf9bc151d85638a
noconnor <noconnorie@protonmail.com>
2018-05-31 11:08:31 +0100

    Adding min heap implementation

--- kata_data_structures/README.md
+++ kata_data_structures/README.md
@@ -1,8 +1,9 @@
 ## Objective 
 
 1. [Implement a hashtable (use chaining)](src/main/java/com/github/noconnor/reference/ChainedHashTable.java)
-2. Implement a heap
-3. Implement a stack
+2. [Implement a min-heap](src/main/java/com/github/noconnor/reference/MinHeap.java)
+3. Implement a min-heap
+4. Implement a stack
 
 <br>
 
@@ -20,3 +21,33 @@ https://algs4.cs.princeton.edu/34hash/
 **Heap:**
 
 * [Binary Heap](https://www.cs.cmu.edu/~adamchik/15-121/lectures/Binary%20Heaps/heaps.html)
+
+A binary heap is a complete binary tree which satisfies the heap ordering property. The ordering can be one of two types:
+
+* the min-heap property: the value of each node is **greater** than or equal to the value of its parent, with the minimum-value element at the root.
+* the max-heap property: the value of each node is **less than** or equal to the value of its parent, with the maximum-value element at the root.
+
+A heap is not a sorted structure and can be regarded as partially ordered.
+
+Heap height `O(log n)`
+
+A heap is useful data structure when you need to remove the object with the highest (or lowest) priority. 
+A common use of a heap is to implement a priority queue.
+
+The root is the second item in the array. 
+We skip the index zero cell of the array for the convenience of implementation. 
+Consider k-th element of the array:
+
+* its left child is located at 2*k index 
+* its right child is located at 2*k+1. index 
+* its parent is located at k/2 index
+
+*min-heap*
+
+* Insert
+  * Add element to end of heap
+  * Compare new element with parent, if less than, swap with parent
+  * repeat comparison step until at root
+  * Worst case running time = `O(log n)`
+* Delete
+  *  
---
+++ kata_data_structures/src/main/java/com/github/noconnor/reference/MinHeap.java
@@ -0,0 +1,130 @@
+package com.github.noconnor.reference;
+
+public class MinHeap<E extends Comparable<? super E>> {
+
+    private E[] heap;
+    private int size = 0;
+
+    public MinHeap(int capacity) {
+        heap = (E[]) new Comparable[capacity - 1];
+    }
+
+    public MinHeap(E[] array) {
+        this.heap = (E[]) new Comparable[array.length + 1];
+        this.size = array.length;
+        System.arraycopy(array, 0, heap, 1, array.length);
+        buildHeap();
+    }
+
+
+    public void insert(E value) {
+        if (size == heap.length - 1) {
+            // increase heap size
+        }
+        // Add value to end of heap
+
+        int pos = ++size;
+
+        // percolate up to find position in heap
+        // pos/2 = parent node location
+        for (; pos > 1 && value.compareTo(heap[pos / 2]) < 0; pos = pos / 2) {
+            heap[pos] = heap[pos / 2];
+        }
+        heap[pos] = value;
+    }
+
+
+    public E deleteMin() {
+        // In a min heap, root will be min
+        E min = heap[1];
+        heap[1] = heap[size];
+        heap[size] = null;
+        size--;
+        percolateDown(1);
+        return min;
+    }
+
+    private void buildHeap() {
+        // (size / 2) will be the parent of the last entry in the heap
+        for (int k = size / 2; k > 0; k--) {
+            percolateDown(k);
+        }
+    }
+
+    private void percolateDown(int index) {
+
+        E valueAtIndex = heap[index];
+        int childIndex;
+        int parentIndex = index;
+
+        for (; 2 * parentIndex <= size; parentIndex = childIndex) {
+            childIndex = 2 * parentIndex;
+
+            // find index of smaller of left & right child
+            if (childIndex != size && heap[childIndex].compareTo(heap[childIndex + 1]) > 0) {
+                childIndex++;
+            }
+
+            // Move the child node up if it is smaller than the value at the parent index
+            if (valueAtIndex.compareTo(heap[childIndex]) > 0) {
+                heap[parentIndex] = heap[childIndex];
+            } else {
+                break;
+            }
+        }
+
+        heap[parentIndex] = valueAtIndex;
+    }
+
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder("\n");
+        inOrder(1, builder, 0);
+        return builder.toString();
+    }
+
+    private void inOrder(int nodeIndex, StringBuilder builder, int index) {
+        if (nodeIndex < heap.length && heap[nodeIndex] != null) {
+
+            int nextLevel = index + 1;
+
+            inOrder(2 * nodeIndex, builder, nextLevel);
+
+            if (index == 0) {
+                builder.append("(");
+                builder.append(heap[nodeIndex]);
+                builder.append(")\n");
+            } else {
+                for (int i = 0; i < index; i++) {
+                    builder.append("|\t");
+                }
+                builder.append("|-(");
+                builder.append(heap[nodeIndex]);
+                builder.append(")\n");
+            }
+
+            inOrder(2 * nodeIndex + 1, builder, nextLevel);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        MinHeap<Integer> heap = new MinHeap<>(100);
+        heap.insert(22);
+        heap.insert(34);
+        heap.insert(3);
+        heap.insert(2);
+        heap.insert(10);
+        heap.insert(14);
+
+        System.out.println(heap);
+        heap.deleteMin();
+        System.out.println(heap);
+
+
+        MinHeap<String> stringHeap = new MinHeap<>(new String[]{"d","z","a","f","b","y","a"});
+        System.out.println(stringHeap);
+
+    }
+
+}
